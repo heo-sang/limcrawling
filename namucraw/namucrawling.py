d7fe556ae52387e517ee6edb81a407ee72628be0
heo-sang <daijang35@gmail.com>
2024-05-10 01:09:06 +0900

    init

--- namucraw/namucrawling.py
+++ namucraw/namucrawling.py
@@ -1,6 +1,7 @@
 import requests 
 import json
 import html
+import re
 from bs4 import BeautifulSoup 
 
 url = "https://namu.wiki/w/%EC%9D%B4%EC%83%81(Project%20Moon%20%EC%84%B8%EA%B3%84%EA%B4%80)/%EC%9D%B8%EA%B2%8C%EC%9E%84%20%EC%A0%95%EB%B3%B4"
@@ -10,27 +11,53 @@ response = requests.get(url)
 if response.status_code == 200:
     html = response.content.decode('utf-8','replace') 
     soup = BeautifulSoup(html, 'lxml')
-    #title = soup.select('#app > div > div.YjvyFted > div._0qEOfn5o > div > div.tWrauFr\+ > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1)')
-    #print(soup.select('#app > div > div.YjvyFted > div._0qEOfn5o > div > div.tWrauFr\+ > div > div:nth-child(2) > div > div > div:nth-child(14) > div:nth-child(1) '))
-    # parent_tag = soup.find('div', {"data-v-04926762": ""})
-    # parent_tag.extract()
-    
-    #app > div > div.YjvyFted > div._0qEOfn5o > div > div.tWrauFr\+ > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)
-    # programs = soup.select('div>div>div') 
-    # tagtemp = soup.find_all('#app > div > div.YjvyFted > div._0qEOfn5o > div > div.tWrauFr\+ > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)')
     personality_base = soup.select_one('#app > div > div.k\+RfjiMA > div.tZQDPien > div > div._8cLLQQqW > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1)')
     temp = personality_base
-    # print(type(temp.text)) # str
-    # print(len(temp.text))
-    #temp = temp.replace(']', "]\n")
-#app > div > div.k\+RfjiMA > div.tZQDPien > div > div._8cLLQQqW > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1)
-#app > div > div.YjvyFted > div._0qEOfn5o > div > div.tWrauFr\+ > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1)    #app > div > div.YjvyFted > div._0qEOfn5o > div > div.tWrauFr\+ > div > div:nth-child(2) > div > div > div:nth-child(18) > div:nth-child(1)
+
+
+
+    
+
+
+    
+    content_list = []
+    for content in temp(text=True):
+       if content.strip():  # content가 공백이 아닌 경우에만 추가
+        content_list.append(content.strip() + '\n')
+    result = ''.join(content_list)
+    print(result)
+    with open("t2.html", "w", encoding='utf8') as file:
+      file.write(result) 
     with open("output2.html", "w", encoding='utf8') as file:
       #file.write(str(temp)) # html 형식
       file.write(temp.text) # content만 남기기
+
+    
+
+    pattern = re.compile(r'.*display:inline.*display:none.*')
+    for element in temp.find_all():
+      attrs = element.attrs
+      if 'style' not in attrs : continue
+      if (pattern.match(attrs['style']) 
+          or 'display:inline' not in attrs['style'] and 'display:none' in attrs['style']):
+          element.clear()
+          element.decompose()
+
+
+    
+    print()
+    content_list = []
+    for content in temp(text=True):
+       if content.strip():  # content가 공백이 아닌 경우에만 추가
+        content_list.append(content.strip() + '\n')
+    result = ''.join(content_list)
+    print(result)
+    with open("t1.html", "w", encoding='utf8') as file:
+      file.write(result) 
     with open("yisang_seven_html.html", "w", encoding='utf8') as file:
       file.write(str(temp.prettify())) # html 형식
       #file.write(temp.text) # content만 남기기
+    
+
 else : 
-  
     print(response.status_code)
--- namucraw/teml.py
+++ namucraw/teml.py
@@ -1,46 +1,78 @@
 from bs4 import BeautifulSoup
 
 # HTML 코드 예시 (변수로 받아온 것이라 가정합니다)
+# html_content = """
+# <html>
+# <head>
+# <title>HTML 예시</title>
+# </head>
+# <body>
+# <div>
+#   <p>부모 1</p>
+#   <div>
+#     <p>부모 2</p>
+#     <div>
+#       <p>부모 3</p>
+#       <div>
+#         <p>부모 4</p>
+#         <div>
+#           <p>부모 5</p>
+#           <div>
+#             <p>asdf</p> <!-- 여기서부터 5번째 부모를 삭제할 것입니다 -->
+#           </div>
+#         </div>
+#       </div>
+#     </div>
+#   </div>
+# </div>
+# </body>
+# </html>
+# """
+
+# # BeautifulSoup 객체 생성
+# soup = BeautifulSoup(html_content, 'html.parser')
+
+# # "asdf"가 포함된 요소 찾기
+# target_element = soup.find(text="asdf").parent
+
+# # 5번째 부모 요소 찾기
+# for _ in range(5):
+#     target_element = target_element.parent
+
+# # 찾은 부모 요소 삭제
+# target_element.decompose()
+
+# # 결과 출력
+# print(soup.prettify())
+from bs4 import BeautifulSoup
+
+# 예시 HTML 코드
 html_content = """
 <html>
 <head>
 <title>HTML 예시</title>
 </head>
 <body>
-<div>
-  <p>부모 1</p>
-  <div>
-    <p>부모 2</p>
-    <div>
-      <p>부모 3</p>
-      <div>
-        <p>부모 4</p>
-        <div>
-          <p>부모 5</p>
-          <div>
-            <p>asdf</p> <!-- 여기서부터 5번째 부모를 삭제할 것입니다 -->
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<div data-v-04926762="" style="width:100%;margin:0px -10px;text-align:left;vertical-align:top;display:inline-block;display:none">
+  <p>첫 번째 문단</p>
+</div>
+<div data-v-04926762="" style="width:100%;margin:0px -10px;text-align:left;vertical-align:top;display:none;display:inline">
+  <p>두 번째 문단</p>
 </div>
 </body>
 </html>
 """
-
+import re
 # BeautifulSoup 객체 생성
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# "asdf"가 포함된 요소 찾기
-target_element = soup.find(text="asdf").parent
+pattern = re.compile(r'.*display:inline.*display:none.*')
 
-# 5번째 부모 요소 찾기
-for _ in range(5):
-    target_element = target_element.parent
-
-# 찾은 부모 요소 삭제
-target_element.decompose()
-
+# display:none이 display:inline 뒤에 오는 경우는 삭제하고 반대의 경우는 남김
+for element in soup.find_all():
+    if 'style' in element.attrs:
+      if pattern.match(element.attrs['style']) :
+        element.clear()
+        element.decompose()  # display:none이 display:inline 뒤에 오는 경우는 삭제
 # 결과 출력
 print(soup.prettify())
